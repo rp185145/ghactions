33c9ff88e9cc0843f774c9e006ae74ef1b6f31b1
Ricky Pike <rp185145@ncr.com>
2020-08-18 10:05:46 -0500

    Initial code drop

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rp185145/ghactions
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func hello() {
+	fmt.Println("Hello, GitHub Actions!")
+}
+
+func main() {
+	hello()
+}
